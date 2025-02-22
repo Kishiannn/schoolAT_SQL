cbb963a87edecdb4d75ddc1082a7b91048bef07a
Kishiannn_ <abudachan122@gmail.com>
2025-02-22 15:49:55 +0800

    Update DATAMANIPULATION.sql

--- DATAMANIPULATION.sql
+++ DATAMANIPULATION.sql
@@ -19,3 +19,10 @@ FROM enroll
 INNER JOIN students ON enroll.stuID = students.stuID
 JOIN class on enroll.classNumber = class.classNumber
 WHERE class.classNumber = 'ART103A';
+
+
+SELECT students.firstName, students.StuID, enroll.grade
+FROM students
+INNER JOIN enroll ON students.StuID = enroll.StuID
+INNER JOIN faculty ON students.major = faculty.department
+WHERE faculty.facID = 'F110';
